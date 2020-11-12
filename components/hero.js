54ec8755cf4aafa3eed9fca2a2cf14731d75b69b
gokul.kathirvel <gokul.kathirvel@zohocorp.com>
2020-11-12 10:33:04 +0530

    adds temp hero illustration

--- components/hero.js
+++ components/hero.js
@@ -1,12 +1,15 @@
 import Button from "./button";
+import HeroIllustration from "./hero-illustration";
 
 const Hero = () => {
   return (
-    <div className="flex justify-between container text-right mx-auto items-center mt-10">
+    <div className="flex justify-between container text-right mx-auto items-center mt-10 relative">
+      
       <div className="w-2/5">
         <img src="./assets/images/hero-illustration.png" alt="Hero Illustration" />
       </div>
-      <div className="w-3/5 pl-6">
+      
+      <div className="w-3/5 pl-6" style={{ zIndex: '2' }}>
         <h2 className="font-bold text-3xl">Welcome to Rmaq Trading Co.</h2>
         <h5 className="text-2xl mt-5 pl-16">
           Rafia Mohammed Al Qarni Trading Est. is
@@ -17,8 +20,11 @@ const Hero = () => {
           <Button> Contact Us </Button>
         </div>
       </div>
+
+      <HeroIllustration />
+    
     </div>
   )
-  };
+};
 
   export default Hero;
