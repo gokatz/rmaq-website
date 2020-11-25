56983fc0a411684837defaa163d3b33b61aa7184
gokul.kathirvel <gokul.kathirvel@zohocorp.com>
2020-11-25 08:41:52 +0530

    nav section mobile ui

--- components/header.js
+++ components/header.js
@@ -1,15 +1,39 @@
+import { useState } from "react";
 import Nav from "./nav";
 
 const Header = () => {
+
+  let [canShowMobileMenu, setMenuState] = useState(false);
+
   return (
     <header>
-      <div className="container mx-auto flex justify-between py-6 items-center">
-        <div className="flex items-center text-lg">
-          <img src="./assets/images/logo-bg.png" className="h-20 mr-6" />
-          Rafia Mohammed Al Qarni <br />Trading Est.
+      {/* <div className="text-center">
+        <img src="./assets/images/logo-bg.png" className="sm:hidden h-16 mt-3 inline-block" />
+      </div> */}
+
+      <div className="container px-6 xl:px-0 mx-auto flex justify-between py-6 items-center">
+        <div className="flex items-center text-xs md:text-lg"> 
+          <div className="flex items-center justify-between">
+            <img src="./assets/images/logo-bg.png" className="h-10 md:h-20 mr-3 md:mr-6" />
+            <span> Rafia Mohammed Al Qarni <br />Trading Est. </span>
+          </div>
         </div>
-        <Nav />
+        
+        <div className="hidden lg:block">
+          <Nav />
+        </div>
+        
+        <div className="lg:hidden">
+          <img src="./assets/icons/menu.svg" className="h-5 ml-3 cursor-pointer md:h-6" onClick={() => setMenuState(!canShowMobileMenu)} />
+        </div>
       </div>
+
+      {
+        canShowMobileMenu && 
+        <div className="lg:hidden -m-3">
+          <Nav />
+        </div>
+      }
     </header>
   )
 };
--- components/nav.js
+++ components/nav.js
@@ -1,19 +1,31 @@
 const Nav = () => {
+
+  let navLinks = [{
+    title: 'Home',
+    href: '/'
+  }, {
+    title: 'About Us',
+    href: '/#about-us'
+  }, {
+    title: 'Products',
+    href: '/products'
+  }, {
+    title: 'Contact Us',
+    href: '/#contact-us'
+  }];
+
   return (
-    <nav>
-      <ul>
-        <li> 
-          <a href="/">Home</a> 
-        </li>
-        <li>
-          <a href="/#about-us">About Us</a>
-        </li>
-        <li>
-          <a href="/products">Products</a>
-        </li>
-        <li>
-          <a href="/#contact-us">Contact Us</a>
-        </li>
+    <nav className="text-sm my-5 lg:my-0 lg:text-lg w-full lg:w-auto">
+      <ul className="text-center bg-brand-red lg:bg-transparent lg:text-left">
+        {
+          navLinks.map((link) => {
+            return (
+              <li className="mx-2 p-2 py-3 lg:inline-block cursor-pointer">
+                <a href={link.href}> {link.title} </a>
+              </li>
+            );
+          })
+        }
       </ul>
     </nav>
   )
