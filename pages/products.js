0cab50bc6c6a344c763d7b49fe53387dccdfae4d
gokul.kathirvel <gokul.kathirvel@zohocorp.com>
2020-12-22 22:17:27 +0530

    product page header

--- pages/products.js
+++ pages/products.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Footer from '../components/footer'
 import Header from '../components/header'
 import Product from '../components/product';
+import SectionHeader from '../components/section-header';
 import ToTop from '../components/to-top';
 import products from '../data/products';
 
@@ -21,6 +22,15 @@ export default function Home() {
           <Header />
         </div>
       </main>
+    
+      <div className="-mt-16">
+        <SectionHeader title="Our Line of Products" />
+      </div>
+
+      <div className="text-lg text-center -mt-5 mb-16 px-5">
+        Here is the list of our world class product offerings!<br />
+        Reach out to us for any personalized product requirement, we'll be glad to assist
+      </div>
 
       <div className="container mx-auto text-center my-10">
         <div className="flex flex-wrap justify-around">
